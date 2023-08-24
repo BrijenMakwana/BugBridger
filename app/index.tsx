2febe3d1ab9108d81df12a3f7c06fe68360aee75
Brijen Makwana <brijenma@gmail.com>
2023-08-24 21:59:40 +0530

    fetch data from stackoverflow API

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import axios from "axios";
 import { Redirect, useRouter } from "expo-router";
 
 import { MyStack } from "../components/MyStack";
@@ -5,7 +7,23 @@ import QuestionCard from "../components/QuestionCard";
 import SearchBar from "../components/SearchBar";
 
 export default function Home() {
-  const router = useRouter();
+  const searchQuestion = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.stackexchange.com/2.3/search/advanced?order=desc&sort=activity&q=expo%20metro%20is%20not%20starting&site=stackoverflow"
+      );
+
+      // Handle the response data here
+      console.log("Response data:", response.data.items);
+    } catch (error) {
+      // Handle errors here
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    searchQuestion();
+  });
 
   return (
     <MyStack>
