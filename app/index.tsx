62105901f114b48e7acee1f80f86cad1ceb25299
Brijen Makwana <brijenma@gmail.com>
2023-08-24 15:51:56 +0530

    SearchBar UI

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
-import { useRouter } from "expo-router";
+import { Redirect, useRouter } from "expo-router";
 
 import { MyStack } from "../components/MyStack";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   const router = useRouter();
 
-  return <MyStack></MyStack>;
+  return (
+    <MyStack>
+      <SearchBar />
+    </MyStack>
+  );
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,35 @@
+import { Search } from "@tamagui/lucide-icons";
+import { Button, Input, XStack } from "tamagui";
+
+const SearchBar = () => {
+  return (
+    <XStack
+      alignItems="center"
+      justifyContent="space-between"
+      padding={10}
+    >
+      <Input
+        size="$4"
+        borderWidth={1}
+        backgroundColor="$backgroundTransparent"
+        flex={1}
+        borderColor="$gray10Dark"
+        focusStyle={{
+          borderColor: "$green10Dark"
+        }}
+      />
+
+      <Button
+        alignSelf="center"
+        icon={Search}
+        size="$4"
+        backgroundColor="$green10Dark"
+        marginLeft={10}
+      >
+        Search
+      </Button>
+    </XStack>
+  );
+};
+
+export default SearchBar;
