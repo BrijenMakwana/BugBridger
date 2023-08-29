99eddbdf56287470bf8b3c116f1efc44c8631367
Brijen Makwana <brijenma@gmail.com>
2023-08-29 10:29:15 +0530

    tabs

--- app/index.tsx
+++ app/index.tsx
@@ -1,74 +1,7 @@
-import { useState } from "react";
-import { RefreshControl } from "react-native";
-import { FlashList } from "@shopify/flash-list";
-import axios from "axios";
+import { Redirect } from "expo-router";
 
-import { MyStack } from "../components/MyStack";
-import QuestionCard from "../components/QuestionCard";
-import SearchBar from "../components/SearchBar";
+const index = () => {
+  return <Redirect href="tabs" />;
+};
 
-export default function Home() {
-  const [searchQuestion, setSearchQuestion] = useState("");
-  const [questions, setQuestions] = useState([]);
-  const [isSearching, setIsSearching] = useState(false);
-
-  const getQuestions = async () => {
-    setIsSearching(true);
-    try {
-      const response = await axios.get(
-        "https://api.stackexchange.com/2.3/search/advanced",
-        {
-          params: {
-            q: searchQuestion,
-            order: "desc",
-            sort: "activity",
-            site: "stackoverflow",
-            filter: "!nNPvSNP4(R",
-            key: process.env.EXPO_PUBLIC_API_KEY
-          }
-        }
-      );
-
-      setQuestions(response.data.items);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    } finally {
-      setIsSearching(false);
-    }
-  };
-
-  const clearSearch = () => {
-    setSearchQuestion("");
-    setQuestions([]);
-  };
-
-  return (
-    <MyStack>
-      <SearchBar
-        setSearchQuestion={setSearchQuestion}
-        searchQuestion={searchQuestion}
-        onPress={getQuestions}
-        onClear={clearSearch}
-      />
-
-      <FlashList
-        data={questions}
-        renderItem={({ item }) => (
-          <QuestionCard
-            {...item}
-            isBody
-          />
-        )}
-        estimatedItemSize={50}
-        refreshControl={
-          <RefreshControl
-            refreshing={isSearching}
-            colors={["green"]}
-            progressBackgroundColor="#333333"
-            onRefresh={getQuestions}
-          />
-        }
-      />
-    </MyStack>
-  );
-}
+export default index;
---
+++ app/tabs/_layout.tsx
@@ -0,0 +1,43 @@
+import { Home, Search } from "@tamagui/lucide-icons";
+import { darkColors } from "@tamagui/themes";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: darkColors.green10,
+        tabBarInactiveTintColor: darkColors.gray11,
+        headerShown: false,
+        tabBarLabelStyle: {
+          textTransform: "capitalize"
+        }
+      }}
+    >
+      <Tabs.Screen
+        name="Home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Home
+              size="$1.5"
+              color={color}
+            />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="Search"
+        options={{
+          title: "Ask Question",
+          tabBarIcon: ({ color }) => (
+            <Search
+              size="$1.5"
+              color={color}
+            />
+          )
+        }}
+      />
+    </Tabs>
+  );
+}
