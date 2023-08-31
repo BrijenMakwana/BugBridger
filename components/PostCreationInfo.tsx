e0336313596a73cc02da2e7c861110f30162da67
Brijen Makwana <brijenma@gmail.com>
2023-08-31 10:15:01 +0530

    time added in post creation info

--- components/PostCreationInfo.tsx
+++ components/PostCreationInfo.tsx
@@ -48,7 +48,7 @@ const PostCreationInfo = (props) => {
           fontWeight="500"
         >
           {type === "question" ? "asked" : "answered"} on{" "}
-          {moment(moment.unix(creationDate)).format("ll")}
+          {moment(moment.unix(creationDate)).format("lll")}
         </Text>
       </XStack>
 
