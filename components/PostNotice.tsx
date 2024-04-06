71351367b09c1f9e2d4c21d88d3084ca839e5b83
BrijenMakwana <brijenma@gmail.com>
2024-04-06 15:32:09 +0530

    PostNotice added

---
+++ components/PostNotice.tsx
@@ -0,0 +1,35 @@
+import { Info } from "@tamagui/lucide-icons";
+import { decode } from "html-entities";
+import { Card, Text } from "tamagui";
+
+export interface IPostNotice {
+  body: string;
+  creation_date?: Date;
+  owner_user_id?: number;
+}
+
+const PostNotice = (props: IPostNotice) => {
+  const { body } = props;
+  return (
+    <Card
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="space-between"
+      gap={10}
+      theme="red"
+      padding={15}
+    >
+      <Info />
+
+      <Text
+        fontSize={15}
+        flex={1}
+        lineHeight={22}
+      >
+        {decode(body)}
+      </Text>
+    </Card>
+  );
+};
+
+export default PostNotice;
