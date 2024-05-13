5b36cef0a6d7bf31fd4292135f589cfb5a043f4f
BrijenMakwana <brijenma@gmail.com>
2024-05-13 10:48:31 +0530

    code refactor

--- components/AIGeneratedAnswer.tsx
+++ components/AIGeneratedAnswer.tsx
@@ -11,11 +11,11 @@ interface IAIGeneratedAnswer {
   questionMarkdown: string;
 }
 
+const DISCLAIMER =
+  "The following answer is AI-generated and may not be entirely accurate. Please use discretion when interpreting the information.";
+
 const AIGeneratedAnswer = (props: IAIGeneratedAnswer) => {
   const { questionMarkdown } = props;
-
-  const disclaimer =
-    "The following answer is AI-generated and may not be entirely accurate. Please use discretion when interpreting the information.";
 
   const prompt = `You are an experienced developer tasked with generating an answer in markdown format based on a given question. Here is the prompt: '${questionMarkdown}'. Please proceed to generate the answer in markdown.`;
 
@@ -38,7 +38,7 @@ const AIGeneratedAnswer = (props: IAIGeneratedAnswer) => {
         paddingVertical={15}
         gap={15}
       >
-        <PostNotice body={disclaimer} />
+        <PostNotice body={DISCLAIMER} />
 
         <CustomMarkdown>{answer}</CustomMarkdown>
       </YStack>
